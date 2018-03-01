390488d73da35edd1c342aa76980799e1714e587
Ryan Roemer <ryan@loose-bits.com>
2018-02-28 16:05:02 -0800

    Fix multiple builds.

--- test/webpack2/webpack.config.js
+++ test/webpack2/webpack.config.js
@@ -1,4 +1,13 @@
 "use strict";
 
+const path = require("path");
+const base = require("../webpack1/webpack.config");
+
 // Reuse webpack1
-module.exports = require("../webpack1/webpack.config");
+module.exports = Object.assign({}, base, {
+  context: __dirname,
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "[hash].main.js"
+  }
+});
